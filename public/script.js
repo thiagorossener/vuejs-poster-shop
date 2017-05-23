a62fefbc3ac5e4fb8b64cd44202ac8c3f84c5637
Thiago Rossener <thiago.rossener@gmail.com>
2017-05-23 12:27:25 -0300

    Finishing first version

--- public/script.js
+++ public/script.js
@@ -26,16 +26,18 @@ new Vue({
            }
        },
        onSubmit: function() {
-           this.items = [];
-           this.loading = true;
-           this.$http
-               .get('/search/'.concat(this.newSearch))
-               .then(function (res) {
-                   this.lastSearch = this.newSearch;
-                   this.results = res.data;
-                   this.appendItems();
-                   this.loading = false;
-               });
+           if (this.newSearch.length) {
+               this.items = [];
+               this.loading = true;
+               this.$http
+                   .get('/search/'.concat(this.newSearch))
+                   .then(function (res) {
+                       this.lastSearch = this.newSearch;
+                       this.results = res.data;
+                       this.appendItems();
+                       this.loading = false;
+                   });
+           }
        },
        addItem: function(index) {
            this.total += PRICE;
